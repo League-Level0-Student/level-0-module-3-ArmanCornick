885a3ed8177002bcf43a07dc3ec64555106b7964
league <league@imac-34.attlocal.net>
2023-08-12 12:53:51 -0700

    0.3

---
+++ src/_02_loop_variables/_4_99_bottles/bottles/Bottles.java
@@ -0,0 +1,45 @@
+package _02_loop_variables._4_99_bottles.bottles;
+
+public class Bottles {
+public static void main(String[] args) {
+int x = 99;
+for(int i=0;i<98;i++) {	
+System.out.println(x +" bottles of beer on the wall, "+ x +" bottles of beer.\nTake one down and pass it around, "+ (x-1) +" bottles of beer on the wall.");	
+x --;
+}	
+System.out.println("1 bottle of beer on the wall, 1 bottle of beer.\nTake one down and pass it around, no more bottles of beer on the wall.");	
+System.out.println("no more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.");	
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
+}
